26885ec733b4da6f860c0063281a2d94dfd231fa
Austin Jerry <austin.jerry2001@gmail.com>
2022-02-25 02:08:37 +0400

    fixed user component

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,12 +3,15 @@ import Avatar from '../Avatar/Avatar';
 import classes from './User.module.scss';
 
 const User = (props) => {
+  console.log(props.user);
   return (
     <div className={classes.user}>
       <Avatar />
       <span className={classes.userDetails}>
         <div className={classes.name}>
-          {props.user.username} - {props.user.wallet}
+          {props.user && props.user.username && props.user.wallet
+            ? `${props.user?.username} - ${props.user?.wallet}`
+            : 'username - wallet'}
         </div>
         <div className={classes.time}>{format(props.date)}</div>
       </span>
